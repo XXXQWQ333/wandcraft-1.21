526dc18edf35afcd35cda847794ff76c64f9626f
XXXQWQ333 <13710103155@163.com>
2025-08-07 11:37:53 +0800

    a

---
+++ src/main/java/org/yumu/wand_craft/wand_craft_mod/WandCraft.java
@@ -0,0 +1,107 @@
+package org.yumu.wand_craft.wand_craft_mod;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraft.client.Minecraft;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import org.yumu.wand_craft.wand_craft_mod.util.RegistryHandler;
+
+// 这里的值应该与 META-INF/neoforge.mods.toml 文件中的条目匹配
+@Mod(WandCraft.MODID)
+public class WandCraft {
+    // 在一个公共位置定义模组ID，供所有地方引用
+    public static final String MODID = "wandcraft";
+    // 直接引用slf4j日志记录器
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+
+    // 模组类的构造函数是模组加载时运行的第一段代码。
+    // FML将识别一些参数类型如IEventBus或ModContainer并自动传入它们。
+    public WandCraft(IEventBus modEventBus, ModContainer modContainer) {
+
+        // 注册commonSetup方法用于模组加载
+        modEventBus.addListener(this::commonSetup);
+        // 使用RegistryHandler注册所有内容
+        RegistryHandler.register(modEventBus);
+
+
+
+        // 为我们自己注册服务器和其他我们感兴趣的事件。
+        // 注意，只有当我们希望这个类(WandCraft)直接响应事件时才需要这行代码。
+        // 如果此类中没有用@SubscribeEvent注解的函数(如下方的onServerStarting())，则不要添加这行代码。
+        NeoForge.EVENT_BUS.register(this);
+
+        // 注册物品到创造模式标签页
+//        modEventBus.addListener(this::addCreative);
+
+        // 注册我们模组的ModConfigSpec，以便FML可以为我们创建和加载配置文件
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        // 一些通用设置代码
+        LOGGER.info("HELLO FROM COMMON SETUP");
+
+        if (Config.LOG_DIRT_BLOCK.getAsBoolean()) {
+            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
+        }
+
+        LOGGER.info("{}{}", Config.MAGIC_NUMBER_INTRODUCTION.get(), Config.MAGIC_NUMBER.getAsInt());
+
+        Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
+    }
+
+    // 将示例方块物品添加到建筑方块标签页
+//    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+//        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+//            event.accept(EXAMPLE_BLOCK_ITEM);
+//        }
+//    }
+
+    // 你可以使用SubscribeEvent让事件总线发现要调用的方法
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // 当服务器启动时做一些事情
+        LOGGER.info("HELLO from server starting");
+    }
+
+    // 你可以使用EventBusSubscriber自动注册用@SubscribeEvent注解的类中的所有静态方法
+    @EventBusSubscriber(modid = WandCraft.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    static class ClientModEvents {
+        @SubscribeEvent
+        static void onClientSetup(FMLClientSetupEvent event) {
+            // 一些客户端设置代码
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
